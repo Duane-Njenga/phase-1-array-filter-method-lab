54413ecd741a83b175000ce26bd540837a903b15
Dwayne <dune.njenga@student.moringaschool.com>
2025-03-09 17:44:23 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+const names = ["Alex", "Jordan", "Taylor", "Morgan", "Casey",];
+
+
+function findMatching(array, str) {
+    let matches = array.filter(element => element.toLowerCase() === str.toLowerCase());
+    return matches;
+}
+
+function fuzzyMatch(array, str){
+   return array.filter(element => element.startsWith(str));; 
+}
+
+function matchName(array, str){
+    return array.filter(driver => driver.name === str);  
+
+}
+
+
+
+console.log(matchName(names, "Alex"));
